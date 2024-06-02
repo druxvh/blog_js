78fa0529b377e2cc97c0269427150e533aaf200b
dm <88588404+druxvh@users.noreply.github.com>
2024-06-02 19:20:34 +0000

    added login route, jwt, cookie parser & login functionality

--- client/src/sections/Login.jsx
+++ client/src/sections/Login.jsx
@@ -1,38 +1,57 @@
 import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://127.0.0.1:4000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        setRedirect(true);
+      } else {
+        alert("Wrong credentials");
+      }
+    } catch (error) {
+      console.error("Failed to register user:", error);
+    }
+  };
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
   return (
-    <form className="max-w-sm mx-auto">
+    <form className="max-w-sm mx-auto" onSubmit={loginUser}>
       <h1 className="text-3xl font-semibold font-serif my-8">Login</h1>
       <div className="mb-5">
-        <label
-          
-          className="block mb-2 text-sm font-medium text-gray-900 "
-        >
+        <label className="block mb-2 text-sm font-medium text-gray-900 ">
           Your Username
         </label>
         <input
           type="text"
           value={username}
-          onChange={(e)=>setUsername(e.target.value)}
+          onChange={(e) => setUsername(e.target.value)}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           placeholder="@username"
           required
         />
       </div>
       <div className="mb-5">
-        <label
-         
-          className="block mb-2 text-sm font-medium text-gray-900 "
-        >
+        <label className="block mb-2 text-sm font-medium text-gray-900 ">
           Your Password
         </label>
         <input
           type="password"
           value={password}
-          onChange={(e)=>setPassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value)}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           required
         />
--- server/index.js
+++ server/index.js
@@ -4,20 +4,23 @@ import User from "./models/User.js";
 import express from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT || 4000;
-
 const uri = process.env.MONGO_DB_URI;
+const secret = process.env.JWT_SECRET;
 
 
-
+app.use(cookieParser())
 
 app.use(
   cors({
     // Optional additional configuration for cors
     origin: process.env.FRONT_END_URL, // frontend's URL
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
   })
 );
 
@@ -36,15 +39,50 @@ app.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
   try {
-  
-    const hashedPassword = await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS));
-  
+    const hashedPassword = await bcrypt.hash(
+      password,
+      parseInt(process.env.SALT_ROUNDS)
+    );
+
     const userDoc = await User.create({ username, password: hashedPassword });
-  
+
     res.status(201).json(userDoc);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const userDoc = await User.findOne({ username });
+    if (!userDoc) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, userDoc.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    //Logged In
+    const token = jwt.sign(
+      {
+        id: userDoc._id,
+        username,
+      },
+      secret,
+      { expiresIn: "3d" }
+    );
+
+    res.cookie('token', token, {httpOnly: true}) //saves the jwt token to cookies
+    res.status(201).json(token);
+
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ error: "Error during login" });
   }
 });
 
