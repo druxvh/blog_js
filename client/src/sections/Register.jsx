a1b2cd0b40506f4a8193c1b79f97eade44cbaf33
dm <88588404+druxvh@users.noreply.github.com>
2024-06-02 07:27:37 +0000

    added bcrypt, connected mongoDB with mongoose

--- client/src/sections/Register.jsx
+++ client/src/sections/Register.jsx
@@ -1,21 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
-  return (
-    <form class=" max-w-sm mx-auto">
-  <h1 className='text-3xl font-semibold font-serif my-8'>Register</h1>
-  <div class="mb-5">
-    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" required />
-  </div>
-  <div class="mb-5">
-    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Your password</label>
-    <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
-  </div>
-  
-  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Create your account</button>
-</form>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Register
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://127.0.0.1:4000/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log('User registered:', data);
+    } catch (error) {
+      console.error('Failed to register user:', error);
+    }
+  };
+
+  return (
+    <form className=" max-w-sm mx-auto" onSubmit={registerUser}>
+      <h1 className="text-3xl font-semibold font-serif my-8">Register</h1>
+      <div className="mb-5">
+        <label
+          
+          className="block mb-2 text-sm font-medium text-gray-900 "
+        >
+          Your Username
+        </label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          placeholder="@username"
+          required
+        />
+      </div>
+      <div className="mb-5">
+        <label
+        
+          className="block mb-2 text-sm font-medium text-gray-900 "
+        >
+          Your Password
+        </label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          required
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+      >
+        Create your account
+      </button>
+    </form>
+  );
+};
+
+export default Register;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,53 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT
+import "dotenv/config";
+import mongoose from "mongoose";
+import User from "./models/User.js";
+import express from "express";
+import cors from "cors";
+import bcrypt from "bcrypt";
 
-app.get('/test', (req, res) => {
-    res.json('Hello World!')
+const app = express();
+const port = process.env.PORT || 4000;
+
+const uri = process.env.MONGO_DB_URI;
+
+
+
+
+app.use(
+  cors({
+    // Optional additional configuration for cors
+    origin: process.env.FRONT_END_URL, // frontend's URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
+);
 
-  app.listen(port)
+app.use(express.json());
+
+//MongoDB connection
+mongoose
+  .connect(uri)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit process with failure
+  });
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+  
+    const hashedPassword = await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS));
+  
+    const userDoc = await User.create({ username, password: hashedPassword });
+  
+    res.status(201).json(userDoc);
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on the port: ${port}`);
+});
