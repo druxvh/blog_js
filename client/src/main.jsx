e5f502f1b63d0ed1d5723926af26b0e19df4ee0a
dm <88588404+druxvh@users.noreply.github.com>
2024-06-05 18:59:49 +0000

    added usercontext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,42 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 //Pages, Components & Sections
-import Layout from './sections/Layout.jsx'
-import Content from './sections/Content.jsx'
-import Register from './sections/Register.jsx'
-import Login from './sections/Login.jsx'
-
-
-
+import Layout from "./sections/Layout.jsx";
+import Content from "./sections/Content.jsx";
+import Register from "./sections/Register.jsx";
+import Login from "./sections/Login.jsx";
+import { UserContextProvider } from "./context/UserContext.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout/>,
+    element: <Layout />,
     children: [
       {
         index: true,
-        element: <Content/>
+        element: <Content />,
       },
       {
         path: "/login",
-        element: <Login/>
+        element: <Login />,
       },
       {
         path: "/register",
-        element: <Register/>
-      }
-    ]
+        element: <Register />,
+      },
+    ],
   },
-  
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
-   <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <UserContextProvider>
+      <RouterProvider router={router} />
+    </UserContextProvider>
+  </React.StrictMode>
+);
